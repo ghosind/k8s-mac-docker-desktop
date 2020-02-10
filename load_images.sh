ab7861d31a348fce862d9062b8e839784af94722
ghosind <ghosind@gmail.com>
2020-02-10 23:41:36 +0800

    Update load_images.sh

--- load_images.sh
+++ load_images.sh
@@ -10,6 +10,14 @@ fi
 
 K8S_VERSION=$(kubectl version | cut -d "\"" -f 6 | head -n 1)
 
+dir="./images/$K8S_VERSION"
+
+if [ ! -d $dir ]
+then
+  echo "Unknown kubernetes version."
+  exit 1
+fi
+
 if [ -z $G $GCR_MIRROR ]
 then
   GCR_MIRROR=gcr.azk8s.cn/google_containers
@@ -20,19 +28,33 @@ then
   QUAY_MIRROR=quay.azk8s.cn
 fi
 
-file="./images/${K8S_VERSION}.txt"
+sources=(
+  GCR
+)
 
-if [ -f $file ]
-then
-  while read -r value
-  do
-    docker pull $GCR_MIRROR/$value
-    docker tag $GCR_MIRROR/$value k8s.gcr.io/$value
-    docker rmi $GCR_MIRROR/$value
-  done < "$file"
-else
-  echo "Unknown kubernetes version."
-  exit 1
-fi
+for source in ${sources[@]}
+do
+  mirror_name=${source}_MIRROR
+  mirror=${!mirror_name}
+  file="$dir/$(tr '[:upper:]' '[:lower:]' <<< $source).txt"
+
+  if [ -z $mirror ]
+  then
+    echo "Invalid $source mirror address"
+    continue
+  fi
+
+  if [ -f $file ]
+  then
+    while read -r value
+    do
+      docker pull $mirror/$value
+      docker tag $mirror/$value k8s.gcr.io/$value
+      docker rmi $mirror/$value
+    done < "$file"
+  else
+    continue
+  fi
+done
 
 exit 0
